6f40125de735e91ae3a346300fd12e3969d0478f
aquaborn679 <79850051+aquaborn679@users.noreply.github.com>
2025-06-27 15:13:16 +0500

    added files

---
+++ Assets/BulletSpawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    AudioSource bulletSound;
+    public float BulletVelocity = 20f;
+
+    void Start()
+    {
+        bulletSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(
+                BulletPrefab, transform.position, transform.rotation);
+            newBullet.GetComponent<Rigidbody>().linearVelocity =
+                transform.forward * BulletVelocity;
+
+            bulletSound.Play();
+        }
+    }
+}
